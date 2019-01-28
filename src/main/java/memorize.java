464ab4ecd8603be166e117ff5febdf48ee9aa7c3
Josiahngugi <mainajosiahdev@gmail.com>
2019-01-28 02:36:50 -0800

    Memorize function

--- src/main/java/memorize.java
+++ src/main/java/memorize.java
@@ -1,6 +1,17 @@
 public class memorize {
-    public void testMethod2() {
+
+//    memorize function
+    public void memorize() {
+        this.originalFunction();
+    }
+//    memorized function
+    public void originalFunction() {
         System.out.println("original method");
 
+    }
+
+    public static void main(String[] args) {
+        memorize t = new memorize();
+        t.memorize();
     }
 }
